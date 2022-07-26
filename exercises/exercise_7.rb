3540bc1c7fcea9a4029ad1194c2e0587ed2e9328
Jesse Shen <jly.shen@gmail.com>
2022-07-26 23:32:40 +0000

    complete

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,15 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Please enter a store name:"
+
+new_store = gets.chomp
+
+@new_store = Store.create(name: new_store)
+
+errors = @new_store.errors.full_messages
+
+errors.each do |error|
+  puts error
+end
